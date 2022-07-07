0352b03aa5ff00dac0a89f1acb0f171417033a64
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2022-07-07 16:38:48 +0100

    App component restructured

--- auto-rc-primary/src/components/App.js
+++ auto-rc-primary/src/components/App.js
@@ -1,9 +1,34 @@
-import Styles from "./styles/Styles.css"
-import React from "react"
+import Styles from "./styles/Styles.css";
+import React, { useState } from "react";
 import LoginLayOut from "./LoginLayOut";
+import Dashboard from "./Dashboard";
+import StudentLogin from "./StudentLoginForm";
+import StudentSignin from "./StudentSignin";
+import Dropdown from "./Dropdown";
 
+const classes = [
+  { name: "One", value: 1 },
+  { name: "Two", value: 2 },
+  { name: "Three", value: 3 },
+  { name: "Four", value: 4 },
+  { name: "Five", value: 5 },
+  { name: "Six", value: 6 },
+  { name: "Select Class", value: 0 },
+];
 function App() {
+  const [selected, setSelected] = useState(classes[6]);
   return (
+    //   <Dropdown
+    //     classes={classes}
+    //     selected={selected}
+    //     onSelectedChange={setSelected}
+    //   />
+    // );
+    // <StudentSignin
+    //   classes={classes}
+    //   selected={selected}
+    //   onSelectedChange={setSelected}
+    // />
     <LoginLayOut/>
   );
 }
